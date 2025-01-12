cc25cfee605f72fbeb29cfe62db3d1e35902e94f
Dhruvipatel03 <dhruvipatel1033@gmail.com>
2025-01-12 18:09:56 +0530

    hey

--- code.py
+++
@@ -1,9 +0,0 @@
-def wordcount(word, string):
-    count = 0
-    for character in string:
-        if character == word:
-            count += 1
-    return count
-word = "a"
-string = "amaaam"
-print(f"charcters: {wordcount(word, string)}")
