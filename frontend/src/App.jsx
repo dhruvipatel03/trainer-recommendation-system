eef8d283715d35c0a9bf9d09f98073176faeb771
Chahnapatel09 <chahnanpatel@gmail.com>
2025-01-13 16:06:06 +0530

    changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Trainers from './pages/Trainers'
 
 const App = () => {
   return (
     <div className='mx-4 sm:mx-[10%]'>
       <Routes>
-       <Route path="/" element={<Home />} />
+        <Route path='/' element = {<Home />} />
+        <Route path='/trainers' element = {<Trainers />} />
       </Routes>
     </div>
   )
