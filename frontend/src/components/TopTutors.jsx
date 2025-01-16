824ee4df40467840c74519e6db9c3123876075ff
Chahnapatel09 <chahnanpatel@gmail.com>
2025-01-17 01:40:03 +0530

    all

--- frontend/src/components/TopTutors.jsx
+++ frontend/src/components/TopTutors.jsx
@@ -3,9 +3,9 @@ import { tutors } from '../assets/assets'
 
 const TopTutors = () => {
   return (
-    <div className='flex flex-col items-centre gap-4 my-10 text-gray-900 md:mx-10'>
+    <div className='flex flex-col items-center gap-4 my-10 text-gray-900 md:mx-10'>
       <h1 className='text-3xl font-medium' >Top Tutors to Book</h1>
-      <p className='sm:w-1/3 text-centre text-sm' >Simply browse through our extensive list of trusted tutors.</p>
+      <p className='sm:w-1/3 text-center text-sm' >Simply browse through our extensive list of trusted tutors.</p>
      <div className='w-full grid grid-cols-auto gap-4 pt-5 gap-y-6 px-3 sm:px-0'>
         {tutors.slice(0,10).map((item, index)=>(
           <div className='border border-blue-200 rounded-xl overflow-hidden cursor-pointer hover:tranlate-y-[-10px] transition-all duration-500' key={index} >
@@ -14,13 +14,13 @@ const TopTutors = () => {
                 <div className='flex items-centre gap-2 text-sm text-center text-green-500'>
                     <p className='w-2 h-2 bg-green-500 rounded-full'></p><p>Available</p>
                 </div>
-                <p>{item.name}</p>
-                <p>{item.speciality}</p>
+                <p className='text-gray-900 text-lg font-medium' >{item.name}</p>
+                <p className='text-gray-600 text-sm'>{item.speciality}</p>
             </div>
           </div>
         ))}
       </div> 
-      <button>more</button>
+      <button className='bg-blue-50 text-gray-600 px-6 py-2 rounded-full mt-10'>more</button>
     </div>
   )
 }
