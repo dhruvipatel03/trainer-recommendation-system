5097a1fd84256104566b42ef6f2de7de7ead5771
Chahnapatel09 <chahnanpatel@gmail.com>
2025-01-17 14:36:05 +0530

    all

--- frontend/src/components/TopTutors.jsx
+++ frontend/src/components/TopTutors.jsx
@@ -12,7 +12,7 @@ const TopTutors = () => {
       <h1 className='text-3xl font-medium' >Top Tutors to Book</h1>
       <p className='sm:w-1/3 text-center text-sm' >Simply browse through our extensive list of trusted tutors.</p>
      <div className='w-full grid grid-cols-auto gap-4 pt-5 gap-y-6 px-3 sm:px-0'>
-        {tutors.slice(0,10).map((item, index)=>(
+        {tutors.slice(0,8).map((item, index)=>(
           <div onClick={()=> navigate(`/appointment/${item._id}`)} className='border border-blue-200 rounded-xl overflow-hidden cursor-pointer hover:tranlate-y-[-10px] transition-all duration-500' key={index} >
             <img className='bg-blue-50' src={item.image} alt="" />
             <div className='p-4'>
