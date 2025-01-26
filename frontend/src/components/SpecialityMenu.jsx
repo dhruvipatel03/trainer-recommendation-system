62a92e0fc7fdbfd4e00ea6278b98a90d3d2df955
Chahnapatel09 <chahnanpatel@gmail.com>
2025-01-26 15:10:45 +0530

    al

--- frontend/src/components/SpecialityMenu.jsx
+++ frontend/src/components/SpecialityMenu.jsx
@@ -8,7 +8,7 @@ const SpecialityMenu = () => {
         <h1 className='text-2xl font-medium'>Find by Speciality</h1>
         <p className='sm:w-1/2 text-center text-sm'>Simply browse through our extensive list of trusted tutors, schedule your appointment hassle-free.</p>
         <div className='flex sm:justify-center gap-4 pt-5 w-full overflow-scroll'>
-            {specialityData.map((item, index)=>(
+            {specialityData.slice(0,6).map((item, index)=>(
               <Link className='flex flex-col items-center text-xs cursor-pointer flex-shrink-0 hover:translate-y-[-10px] transition-all duration-500' key={index} to={`/tutors/${item.speciality}`}>
                  <img className='w-14 sm:w-20 mb-2' src={item.image} alt="" />
                  <p>{item.speciality}</p>
