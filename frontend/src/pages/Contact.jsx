1bf38ab02332e47269c03f3018eb182ef735dd50
Chahnapatel09 <chahnanpatel@gmail.com>
2025-01-26 21:19:17 +0530

    all

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -12,13 +12,13 @@ const Contact = () => {
         <div className='flex justify-center items-center pt-10'>
         <div className='flex flex-col justfy-center md:flex-row gap-10 text-sm'>
           <img className='w-full md:max-w-[300px]' src={assets.contact_img} alt="" />
-          <div className='flex flex-col justify-center items-start gap-3'>
-            <p>OUR OFFICE</p>
-            <p>456, Ashram Road, Near Paldi, <br />Ahmedabad, India</p>
-            <p>Tel: +91 98756 78982 <br />Email: guidegrid@gmail.com</p>
-            <p>CAREERS AT GUIDEGRID</p>
-            <p>Learn more about our teams and job openings.</p>
-            <button>Explore Jobs</button>
+          <div className='flex flex-col justify-center items-start gap-2'>
+            <p className='font-semibold text-lg text-gray-600'>OUR OFFICE</p>
+            <p className='text-gray-500'>456, Ashram Road, Near Paldi, <br />Ahmedabad, India</p>
+            <p className='text-gray-500'>Tel: +91 98756 78982 <br />Email: guidegrid@gmail.com</p>
+            <p className='font-semibold text-lg text-gray-600'>CAREERS AT GUIDEGRID</p>
+            <p className='text-gray-500'>Learn more about our teams and job openings.</p>
+            <button className='border border-black px-4 py-1 text-sm hover:bg-black hover:text-white transition-all duration-500'>Explore Jobs</button>
           </div>
         </div>
         </div>
