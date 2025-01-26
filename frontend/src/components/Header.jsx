7f673e8a4d50393fc0eae0627789ff539d903a75
Chahnapatel09 <chahnanpatel@gmail.com>
2025-01-26 16:07:12 +0530

    all

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,8 +1,5 @@
 import React from 'react'
-import group_profiles from '../assets/group_profiles.png';
-import arrow_icon from '../assets/arrow_icon.svg';
-import tutors from '../assets/tutors.png';
-
+import { assets } from '../assets/assets';
 
 const Header = () => {
   return (
@@ -15,20 +12,20 @@ const Header = () => {
             </p>
 
             <div className='flex flex-col md:flex-row items-center gap-3 text-white text-sm font-light'>
-              <img className='w-20' src={group_profiles} alt="" />
+              <img className='w-20' src={assets.group_profiles} alt="" />
               {/* <p className="text-sx md:text-base mt-1"> */}
               <p className="text-xs md:text-sm mt-1">
                 Simply browse through our extensive list of <br className='hidden sm:block' /> trusted tutors, schedule your appointment hassle-free.
               </p>
             </div>
             <a href="#speciality"  className="flex items-center gap-2 bg-white px-8 py-3 rounded-full text-gray-600 text-sm hover:scale-105 transition-all duration-300">
-               Book appointment <img className='w-3' src={arrow_icon} alt="" />
+               Book appointment <img className='w-3' src={assets.arrow_icon} alt="" />
             </a>
         </div>
 
         {/* ------Right Side------- */}
             <div className='md:w-1/2 relative'>
-              <img  style={{ width: '65%' }} className='md:absolute bottom-0 left-1/2 transform -translate-x-1/2 h-auto rounded-lg' src={tutors} alt="" />
+              <img  style={{ width: '65%' }} className='md:absolute bottom-0 left-1/2 transform -translate-x-1/2 h-auto rounded-lg' src={assets.tutors} alt="" />
             </div>
     </div>
   )
