cd44d9074e031167ab272cae278e4c850c702754
Chahnapatel09 <chahnanpatel@gmail.com>
2025-01-28 13:29:11 +0530

    al

--- frontend/src/pages/Myappointments.jsx
+++ frontend/src/pages/Myappointments.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { AppContext } from '../context/AppContext'
 
 const Myappointments = () => {
+
+  const { tutors } = useContext(AppContext)
+
   return (
     <div>
-        
+        <p>My appointments</p>
+        <div>
+          {doctors.slice(0,2).map((item, index)=>(
+            <div key={index}>
+              <div>
+                <img src={item.image} alt="" />
+              </div>
+            </div>
+          ))}
+        </div>
     </div>
   )
 }
