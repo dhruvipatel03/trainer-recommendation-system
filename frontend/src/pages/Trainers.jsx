7992b584141db3d68405bc175fd153cd5d5d50df
Dhruvipatel03 <dhruvipatel1033@gmail.com>
2025-01-28 14:14:30 +0530

    change

--- frontend/src/pages/Trainers.jsx
+++ frontend/src/pages/Trainers.jsx
@@ -6,6 +6,7 @@ import { AppContext } from '../context/AppContext'
 const Trainers = () => {
   const {speciality}=useParams()
   const [filterTrain, setFilterTrain] = useState([])
+  const [showFilter , setShowFilter] = useState(false)
   const navigate =useNavigate()
   const { tutors = [] } = useContext(AppContext);
 
@@ -23,7 +24,13 @@ const applyFilter = () => {
     <div>
       <p className='text-gray-600'>Brows through the trainers speciality</p>
       <div className='flex flex-col sm:flex-row items-start gap-5 mt-5'>
-      <div className="flex flex-col gap-4 text-sm text-gray-600">
+      <button
+  className={`py-1 px-3 border rounded text-sm transition-all sm:hidden ${showFilter ? 'bg-primary text-white' : ''}`}
+  onClick={() => setShowFilter((prev) => !prev)}
+>
+  Filters
+</button>
+      <div className={`flex-col gap-4 text-sm text-gray-600 ${showFilter ? 'flex': 'hidden sm:flex'}`}>
   <p
     onClick={() => (speciality === 'Biology' ? navigate('/tutors') : navigate('/tutors/Biology'))}
     className={`W-[100vw] sm:w-[16VW] pl-3 py-1.5 pr-16 border border-gray-300 rounded transition-all cursor-pointer ${
@@ -97,7 +104,7 @@ const applyFilter = () => {
     Computer Science
   </p>
 </div>
-        <div className='w-full grid grid-cols-3 gap-4 gap-y-6'>
+        <div className='w-full grid grid-cols-auto gap-4 gap-y-6'>
           {
             filterTrain.map((item, index)=>(
               <div onClick={()=> navigate(`/appointment/${item._id}`)} className='border border-blue-200 rounded-xl overflow-hidden cursor-pointer hover:tranlate-y-[-10px] transition-all duration-500' key={index} >
