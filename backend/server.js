2acf28a34ce78b88e425ab5163433fc9a9cf450b
Dhruvipatel03 <dhruvipatel1033@gmail.com>
2025-02-05 14:48:24 +0530

    ..

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ import express from 'express';
 import cors from 'cors';
 import connectDB from './config/mongodb.js'; 
 import connectCloudinary from './config/cloudinary.js';
-import adminRouter from './routes/adminroute.js';
+import adminRouter from './routes/adminRoute.js';
 
 
 //app config
